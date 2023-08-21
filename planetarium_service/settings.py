2f848320bcb813ccda4f85287f0a93c3d4b97b84
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-08-21 14:42:21 +0300

    Created new app user & added user app in setting.py & added db.sqlite3 in .gitignore

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "planetarium",
     "rest_framework",
+    "user",
 
 ]
 
@@ -101,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
