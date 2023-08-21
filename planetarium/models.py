3e3c676ab6ed49f6b235d30fe2b4da565e8cfff9
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-08-21 15:07:56 +0300

    Added str methods for models

--- planetarium/models.py
+++ planetarium/models.py
@@ -5,16 +5,25 @@ from django.db import models
 class ShowTheme(models.Model):
     name = models.CharField(max_length=63)
 
+    def __str__(self):
+        return self.name
+
 
 class AstronomyShow(models.Model):
     title = models.CharField(max_length=63)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 
 class PlanetariumDome(models.Model):
     name = models.CharField(max_length=63)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class ShowSession(models.Model):
@@ -26,14 +35,28 @@ class ShowSession(models.Model):
     )
     show_time = models.DateTimeField()
 
+    def __str__(self):
+        return (f"Astronomy Show: {self.astronomy_show} | "
+                f"Show time: {self.show_time}")
+
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"User: {self.user} | {self.created_at}"
+
 
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
-    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    show_session = models.ForeignKey(
+        ShowSession, on_delete=models.CASCADE, related_name="ticket"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="ticket"
+    )
+
+    def __str__(self):
+        return f"{self.show_session} | Row: {self.row} | Seat: {self.seat}"
