5955e2e6a95bc5a81279403bb0b03292aaaf39df
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-08-22 14:14:36 +0300

    Created endpoints for views

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,7 +1,23 @@
 from rest_framework import routers
 
+from planetarium.views import (
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+    PlanetariumDomeViewSet,
+    ShowSessionViewSet,
+    ReservationViewSet,
+    TicketViewSet,
+)
+
 app_name = "planetarium"
 
 router = routers.DefaultRouter()
+
+router.register("show_themes", ShowThemeViewSet)
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("domes", PlanetariumDomeViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("tickets", TicketViewSet)
 
 urlpatterns = router.urls
