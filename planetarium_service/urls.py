1de9a053caee90873e8ef73e757c93b978789bbb
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-08-22 13:42:46 +0300

    Rewrote code & fixed bug

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("planetarium.urls"), app_name="planetarium")
+    path("api/planetarium/", include("planetarium.urls", namespace="planetarium"))
 ]
