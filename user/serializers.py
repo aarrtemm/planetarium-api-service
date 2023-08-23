37fa8f8832fd2c20d624e0ffec4e15555de693a2
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-08-23 13:55:20 +0300

    Fixed code

--- user/serializers.py
+++ user/serializers.py
@@ -5,8 +5,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "password", "email", "is_staff")
-        read_only_fields = ("id", "password")
+        fields = ("id", "password", "email", "is_staff")
+        read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def create(self, validated_data):
