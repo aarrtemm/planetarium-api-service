35c8fe1384ddca2bc408be8c041364d4fabeb387
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-08-25 13:26:27 +0300

    Changed code fot movie_image_filter_path

--- planetarium/models.py
+++ planetarium/models.py
@@ -18,7 +18,7 @@ def movie_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/shows/", filename)
+    return os.path.join("uploads", "shows", filename)
 
 
 class AstronomyShow(models.Model):
